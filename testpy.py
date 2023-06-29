64439b56d65696cdf8419bfc241f1e7c55d19ff4
Ashuayd <129171345+Ashuayd@users.noreply.github.com>
2023-06-29 16:24:39 +0200

    Update testpy.py

--- testpy.py
+++ testpy.py
@@ -1,7 +1,10 @@
-import sys
+#import sys
 #sys.path += ['../src']
 
-from test_func import *
+#from test_func import *
+
+def func(x):
+    return x + 2
 
 def test_answer():
     assert func(3) == 5
